aa416d79b2528b3ef6e1dae1070cbd08aa673e83
toluwani <toluwani@ut.ee>
2023-12-20 11:59:39 +0200

    refactor: Improve XAI functionality for AC models by introducing dedicated label lists

--- src/customComponents/util/utility.ts
+++ src/customComponents/util/utility.ts
@@ -1,4 +1,17 @@
- export const ConvertTimeStamp = (timestamp: number): string => {
+import {AC_OUTPUT_LABELS, AD_OUTPUT_LABELS, AD_OUTPUT_LABELS_XAI} from "../../constants";
+export const ConvertTimeStamp = (timestamp: number): string => {
     const date = new Date(timestamp);
     return date.toUTCString();
 };
+
+export const isACApp = (app : string) => app === 'ac';
+export const isACModel = (modelId : string) => modelId && modelId.startsWith('ac-');
+export const getLabelsListApp = (app: string) => {
+  return isACApp(app) ? AC_OUTPUT_LABELS : AD_OUTPUT_LABELS;
+}
+export const getLabelsListAppXAI = (app: string) => {
+  return isACApp(app) ? AC_OUTPUT_LABELS : AD_OUTPUT_LABELS_XAI;
+}
+export const getLabelsListXAI = (modelId: string) => {
+  return isACModel(modelId) ? AC_OUTPUT_LABELS : AD_OUTPUT_LABELS_XAI;
+}
