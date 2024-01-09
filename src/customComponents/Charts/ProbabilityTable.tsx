47e7c03bf7a3506f6707c2ac8a6ad329fea8d9b4
toluwani <toluwani@ut.ee>
2024-01-09 10:25:38 +0200

    Refactor: Added the wrapper Spatial services

--- src/customComponents/Charts/ProbabilityTable.tsx
+++ src/customComponents/Charts/ProbabilityTable.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Make sure to import Bootstrap CSS
 
 interface TableDataItem {
     key: string;
     value: number;
     label: string;
-    probability: number
+    probability: number;
 }
 
 interface TableDataComponentProps {
     data: TableDataItem[];
 }
 
-const ProbabilityTable: React.FC<TableDataComponentProps>= ({data}) => {
+const ProbabilityTable: React.FC<TableDataComponentProps> = ({data}) => {
+    const formatProbability = (probability: number) => `${(probability * 100).toFixed(2)}%`;
 
-
-  // Data provided
-  // const data = [
-  //   { key: 0, label: "Web", probability: "0.718533" },
-  //   { key: 1, label: "Interactive", probability: "0.000109" },
-  //   { key: 2, label: "Video", probability: "0.281357" }
-  // ];
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Key</th>
-          <th>Label</th>
-          <th>Probability</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item: TableDataItem) => (
-          <tr key={item.key}>
-            <td>{item.key}</td>
-            <td>{item.label}</td>
-            <td>{item.probability}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    return (
+        <div className="table-responsive">
+            <table className="table table-hover">
+                <thead className="table-light">
+                <tr>
+                    <th>Key</th>
+                    <th>Label</th>
+                    <th>Probability</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((item) => (
+                    <tr key={item.key} className={item.probability > 0.5 ? 'table-success' : ''}>
+                        <td>{item.key}</td>
+                        <td>{item.label}</td>
+                        <td>{formatProbability(item.probability)}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default ProbabilityTable;
--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import { useOktaAuth } from "@okta/okta-react";
-import { useMenuData } from './MenuItem'; // Ensure this hook is defined and exports menu data
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import {useOktaAuth} from "@okta/okta-react";
+import {useMenuData} from './MenuItem'; // Ensure this hook is defined and exports menu data
 import './MenuStyles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -16,7 +16,7 @@ type MenuItemType = {
 
 const Sidebar: React.FC = () => {
     const [openSubmenu, setOpenSubmenu] = useState<number | null>(null);
-    const { authState, oktaAuth } = useOktaAuth();
+    const {authState, oktaAuth} = useOktaAuth();
     const menuItems = useMenuData(); // Ensure this returns the correct menu data structure
 
     if (!authState) return <div>Loading...</div>;
@@ -30,14 +30,14 @@ const Sidebar: React.FC = () => {
         await oktaAuth.signOut();
     };
 
-    const MenuItem = ({ item }: { item: MenuItemType }) => (
+    const MenuItem = ({item}: { item: MenuItemType }) => (
         <li key={item.id} className="nav-item text-white my-1 justify-content-start">
             <Link
                 to={(item.subItems?.length ?? 0) > 0 ? "#" : item.path}
                 className="nav-link text-white fs-5"
                 onClick={e => (item.subItems?.length ?? 0) > 0 && toggleSubmenu(e, item.id)}
             >
-                <i className={item.icon} ></i>
+                <i className={item.icon}></i>
                 <span className='ms-3 d-none d-lg-inline'>{item.title}</span>
             </Link>
             {(item.subItems?.length ?? 0) > 0 && (
@@ -56,40 +56,44 @@ const Sidebar: React.FC = () => {
     );
 
     return (
-  <div className='container-fluid'>
-    <div className='row'>
-        <div className='bg-dark col min-vh-100 d-flex flex-column'> {/* Removed justify-content-between */}
-            <div className="p-3 text-white bg-dark">
-                {/* Logo Links */}
-                <Link to='/' className='text-decoration-none text-white ms-3 mt-3 d-none d-lg-block'>
-                    <img src="/Spatial_Logo.png" width="129" height="39.5" className="d-block" alt="Logo" /> {/* Changed to d-block */}
-                </Link>
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='bg-dark col min-vh-100 d-flex flex-column'> {/* Removed justify-content-between */}
+                    <div className="p-3 text-white bg-dark">
+                        {/* Logo Links */}
+                        <Link to='/' className='text-decoration-none text-white ms-3 mt-3 d-none d-lg-block'>
+                            <img src="/Spatial_Logo.png" width="129" height="39.5" className="d-block"
+                                 alt="Logo"/> {/* Changed to d-block */}
+                        </Link>
 
-                <Link to='/' className='text-decoration-none text-white ms-3 mt-3 d-block d-md-none'>
-                    <img src="/Logo.png" className="img-fluid" alt="Logo" style={{ maxWidth: '129px', maxHeight: '39.5px' }} /> {/* Removed d-inline-block align-center */}
-                </Link>
-                <hr className='text-secondary' />
-                <ul className='nav nav-pills flex-column mt-3 text-start'> {/* text-start will align text to the left */}
-                    {menuItems.filter(item => authState.isAuthenticated || !item.secure)
-                        .map(item => <MenuItem item={item} key={item.id} />)}
-                    <li className="nav-item my-1">
-                        {authState.isAuthenticated ? (
-                            <Link to='#' onClick={handleLogout} className="nav-link text-white fs-5">
-                                <i className='bi bi-box-arrow-right'></i>
-                                <span className='ms-3 d-none d-lg-inline'>Logout</span>
-                            </Link>
-                        ) : (
-                            <Link to='/login' className="nav-link text-white fs-5">
-                                <i className='bi bi-box-arrow-in-right'></i>
-                                <span className='ms-3 d-none d-lg-inline'>Login</span>
-                            </Link>
-                        )}
-                    </li>
-                </ul>
+                        <Link to='/' className='text-decoration-none text-white ms-3 mt-3 d-block d-md-none'>
+                            <img src="/Logo.png" className="img-fluid" alt="Logo" style={{
+                                maxWidth: '129px',
+                                maxHeight: '39.5px'
+                            }}/> {/* Removed d-inline-block align-center */}
+                        </Link>
+                        <hr className='text-secondary'/>
+                        <ul className='nav nav-pills flex-column mt-3 text-start'> {/* text-start will align text to the left */}
+                            {menuItems.filter(item => authState.isAuthenticated || !item.secure)
+                                .map(item => <MenuItem item={item} key={item.id}/>)}
+                            <li className="nav-item my-1">
+                                {authState.isAuthenticated ? (
+                                    <Link to='#' onClick={handleLogout} className="nav-link text-white fs-5">
+                                        <i className='bi bi-box-arrow-right'></i>
+                                        <span className='ms-3 d-none d-lg-inline'>Logout</span>
+                                    </Link>
+                                ) : (
+                                    <Link to='/login' className="nav-link text-white fs-5">
+                                        <i className='bi bi-box-arrow-in-right'></i>
+                                        <span className='ms-3 d-none d-lg-inline'>Login</span>
+                                    </Link>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
 
     );
