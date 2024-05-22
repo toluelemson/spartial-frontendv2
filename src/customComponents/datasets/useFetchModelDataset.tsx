81b76a156a8affa01b75bddcaa861771c4ea9d64
elemson <toluwanielemosho@gmail.com>
2024-05-22 13:41:42 +0300

    DRM - dynamically fetch dataset based on the type

--- src/customComponents/datasets/useFetchModelDataset.tsx
+++ src/customComponents/datasets/useFetchModelDataset.tsx
@@ -1,45 +1,51 @@
 import { useEffect, useState } from 'react';
 import Papa from 'papaparse';
+import { requestViewModelDatasets } from "../../api";
 
 interface DataModel {
     resultData: string[];
 }
 
-interface Api {
-    requestViewModelDatasets: (modelId: string, dataType: string) => Promise<string>;
-}
-
-const useFetchModelData = (modelId: string, api: Api) => {
-    const [data, setData] = useState<DataModel | null>(null);
+const useFetchModelDataset = (modelId: string, datasetType: string) => {
+    const [originalDataset, setOriginalDataset] = useState<DataModel>({ resultData: [] });
     const [error, setError] = useState<Error | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchData = async () => {
+            if (!modelId) return;
+
             setLoading(true);
             try {
-                const csvDataString = await api.requestViewModelDatasets(modelId, "train");
+                const csvDataString = await requestViewModelDatasets(modelId, datasetType);
+                if (!csvDataString) return;
+
                 Papa.parse(csvDataString, {
                     complete: (result) => {
+                        if (result.data.length === 0) {
+                            setOriginalDataset({ resultData: [] });
+                            return;
+                        }
+
                         if (Array.isArray(result.data)) {
-                            setData({ resultData: result.data as string[] });
+                            setOriginalDataset({ resultData: result.data as string[] });
+                        } else {
+                            throw new Error('Error parsing data');
                         }
                     },
                     header: true,
                 });
             } catch (err) {
-                setError(err instanceof Error ? err : new Error('Error fetching data'));
+                setError(err instanceof Error ? err : new Error('An error occurred while fetching data'));
             } finally {
                 setLoading(false);
             }
         };
 
-        if (modelId) {
-            fetchData();
-        }
-    }, [api, modelId]); // Ensured that useEffect is dependent on api and modelId
+        fetchData();
+    }, [modelId, datasetType]);
 
-    return { data, error, loading };
+    return { originalDataset, error, loading }; 
 };
 
-export default useFetchModelData;
+export default useFetchModelDataset;
