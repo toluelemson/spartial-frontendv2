e2413d32e6325632ae70418ed864b6ef027d96f0
elemson <toluwanielemosho@gmail.com>
2024-05-30 22:27:45 +0300

    DRM - Disabled files

--- clean.sh
+++
@@ -1,8 +0,0 @@
-rm -rf src/server/logs/*
-rm -rf src/server/mmt/outputs/*
-rm -rf src/server/mmt/pcaps/*
-rm -rf src/server/deep-learning/models/*
-rm -rf src/server/deep-learning/predictions/*
-rm -rf src/server/deep-learning/trainings/*
-rm -rf src/server/deep-learning/attacks/*
-rm -rf src/server/deep-learning/xai/*
