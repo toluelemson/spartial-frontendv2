fc6af4c6f568864e621073181cdcb0c20d1d3348
elemson <toluwanielemosho@gmail.com>
2024-06-17 18:09:59 +0300

    Refactor: Improve layout responsiveness and readability

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import Menu from "././Sidebar/Sidebar";
 import AppRoutes from "../routes/AppRoutes";
 import {Footer} from "./Footer";
+import {Col, Container, Row } from 'react-bootstrap';
 
 const Layout = () => {
     return (
-        <div className="container-fluid">
-            <div className="row ">
-                <div className="col-2 bg-dark">
-                    <Menu/>
-                </div>
-                <div className="col-10">
-                    <AppRoutes/>
-                    <Footer/>
-                </div>
-
-            </div>
-
-        </div>
+        <Container fluid>
+            <Row>
+                <Col lg={2} className="bg-dark p-0">
+                    <Menu />
+                </Col>
+                <Col lg={10} className="p-0">
+                    <AppRoutes />
+                    <Footer />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
