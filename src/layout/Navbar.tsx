da14453ddb13f005b2bc8671da1661142a720c84
elemson <toluwanielemosho@gmail.com>
2024-06-17 18:09:36 +0300

    Refactor: Enhance Navbar responsiveness and user experience

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useOktaAuth } from '@okta/okta-react';
-import { Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Button, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 const NavBar: React.FC = () => {
     const { authState, oktaAuth } = useOktaAuth();
@@ -14,16 +14,17 @@ const NavBar: React.FC = () => {
     return (
         <>
             <Navbar expand="lg" className="bg-dark-custom text-white navbar-light">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        {/* Separate Home link */}
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                        </Nav>
-                        <Nav className="ms-auto">
-                            {!authState.isAuthenticated ? (
-                                <Nav.Link className="btn btn-outline-dark">Sign In</Nav.Link>) : (<>
-                                <NavDropdown title="Network Traffic" id="anomaly-detection-dropdown">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto flex-row flex-lg-column">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                    </Nav>
+                    <Nav className="ms-auto flex-row flex-lg-column">
+                        {!authState.isAuthenticated ? (
+                            <Nav.Link className="btn btn-outline-dark">Sign In</Nav.Link>
+                        ) : (
+                            <>
+                                <NavDropdown title="Network Traffic" id="anomaly-detection-dropdown" className="flex-row flex-lg-column">
                                     <NavDropdown.Item href="#action/3.1">Anomaly detection</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">Predict</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.3">Attack</NavDropdown.Item>
@@ -33,16 +34,18 @@ const NavBar: React.FC = () => {
                                     <NavDropdown.Item href="#metrics">Metrics</NavDropdown.Item>
                                     <NavDropdown.Item href="#build">Build</NavDropdown.Item>
                                 </NavDropdown>
-                                <NavDropdown title="Models" id="models-dropdown">
+                                <NavDropdown title="Models" id="models-dropdown" className="flex-row flex-lg-column">
                                     <NavDropdown.Item href="#models/all">All Models</NavDropdown.Item>
                                     <NavDropdown.Item href="#models/comparison">Models Comparison</NavDropdown.Item>
                                     <NavDropdown.Item href="#models/retraining">Models Retraining</NavDropdown.Item>
                                 </NavDropdown>
-                                <button onClick={handleLogout} className="btn btn-outline-light">Logout</button> </> )
-                            }
-                        </Nav>
-                    </Navbar.Collapse>
-
+                                <Button onClick={handleLogout} className="btn btn-outline-light">
+                                    Logout
+                                </Button>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </>
     )
