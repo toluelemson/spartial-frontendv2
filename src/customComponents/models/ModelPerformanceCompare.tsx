a55fc96986e4bad6bb6eb805cf6736f91df0c014
elemson <toluwanielemosho@gmail.com>
2024-06-17 14:53:37 +0300

    feat: Add ModelPerformanceCompare component for comparing model performance
    - Implement `ModelPerformanceCompare` to compare performance metrics of two models.
    - Use `PerformanceRow` and `TableSection` components to display performance data in tables.
    - Define `PerformanceMetrics` and `classificationLabel` interfaces for structured data handling.
    - Conditionally render performance data for selected models using dynamic column headers.
    - Include commented-out sections for potential future enhancements in performance comparison.

---
+++ src/customComponents/models/ModelPerformanceCompare.tsx
@@ -0,0 +1,77 @@
+import PerformanceRow from "./PerformanceRow";
+import {TableSection} from "./TableSection";
+
+
+interface PerformanceMetrics {
+    metric: string;
+    class0: string;
+    class1: string;
+    class2: string;
+}
+
+interface classificationLabel {
+    label1: string[];
+    label2: string[];
+}
+
+interface ModelPerformanceCompareProps {
+    selectedModelLeft: boolean;
+    selectedModelRight: boolean;
+    dataStatsLeft: PerformanceMetrics[];
+    dataStatsRight: PerformanceMetrics[];
+    classificationLabel: classificationLabel;
+}
+
+const ModelPerformanceCompare: React.FC<ModelPerformanceCompareProps> = ({
+                                                                             selectedModelLeft,
+                                                                             selectedModelRight,
+                                                                             dataStatsLeft,
+                                                                             dataStatsRight,
+                                                                             classificationLabel,
+                                                                         }) => {
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    <TableSection title="Performance" columns={classificationLabel.label1}>
+                        {selectedModelLeft && dataStatsLeft.map((d, index: any) => (
+                            <PerformanceRow key={`left-${index}`} data={d}/>
+                        ))}
+                    </TableSection>
+                </div>
+                <div className="col">
+                    <TableSection title="Performance" columns={classificationLabel.label2}>
+                        {selectedModelRight && dataStatsRight.map((d, index) => (
+                            <PerformanceRow key={`right-${index}`} data={d}/>
+                        ))}
+                    </TableSection>
+                </div>
+                {/*<div className="col">*/}
+                {/*    <TableSection title="Performance" columns={classificationLabel.label1}>*/}
+                {/*        {selectedModelLeft && dataStatsLeft.map((d, index) => (*/}
+                {/*            <PerformanceRow key={`left-${index}`} data={d}/>*/}
+                {/*        ))}*/}
+                {/*    </TableSection>*/}
+                {/*</div>*/}
+            </div>
+            <div className="row">
+                {/*<div className="col">*/}
+                {/*    <TableSection title="Attack Performance" columns={classificationLabel.label1}>*/}
+                {/*        {selectedModelLeft && dataStatsLeft.map((d, index) => (*/}
+                {/*            <PerformanceRow key={`left-${index}`} data={d}/>*/}
+                {/*        ))}*/}
+                {/*    </TableSection>*/}
+                {/*</div>*/}
+                {/*<div className="col">*/}
+                {/*    <TableSection title="Attack Performance" columns={classificationLabel.label2}>*/}
+                {/*        {selectedModelRight && dataStatsRight.map((d, index) => (*/}
+                {/*            <PerformanceRow key={`right-${index}`} data={d}/>*/}
+                {/*        ))}*/}
+                {/*    </TableSection>*/}
+                {/*</div>*/}
+            </div>
+        </>
+    )
+}
+
+export default ModelPerformanceCompare;
