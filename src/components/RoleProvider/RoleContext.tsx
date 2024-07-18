75eae548bd831b5b06532c49d349db8a8075a546
elemson <toluwanielemosho@gmail.com>
2024-07-18 11:54:04 +0300

    Refactor: Remove role selector from main sidebar because it is only used for medical service.

--- src/components/RoleProvider/RoleContext.tsx
+++ src/components/RoleProvider/RoleContext.tsx
@@ -1,53 +1,53 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {createContext, useContext, useState, ReactNode} from "react";
 
 export type Role =
-  // | "user"
-  "business" | "developer" | "auditor" | "medicalExpert" | "endUser";
+// | "user"
+    "business" | "developer" | "auditor" | "medicalExpert" | "endUser";
 
 interface RoleContextProps {
-  roles: Role[];
-  currentService: string;
-  setCurrentService: (service: string) => void;
-  userRole: Role;
-  setUserRole: (role: Role) => void;
+    roles: Role[];
+    currentService: string;
+    setCurrentService: (service: string) => void;
+    userRole: Role;
+    setUserRole: (role: Role) => void;
 }
 
 const RoleContext = createContext<RoleContextProps | undefined>(undefined);
 
 export const RoleProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  const [currentService, setCurrentService] = useState<string>("XAI");
-  const [userRole, setUserRole] = useState<Role>("endUser");
+                                                                    children,
+                                                                }) => {
+    const [currentService, setCurrentService] = useState<string>("XAI");
+    const [userRole, setUserRole] = useState<Role>("endUser");
 
-  const rolesForServices: { [key: string]: Role[] } = {
-    XAI: ["endUser", "business", "developer", "auditor"],
-    Medical: ["endUser", "medicalExpert", "developer"],
-    Network: ["auditor", "developer"],
-    Fairness: ["endUser", "developer"],
-  };
+    const rolesForServices: { [key: string]: Role[] } = {
+        XAI: ["endUser", "business", "developer", "auditor"],
+        Medical: ["endUser", "medicalExpert", "developer"],
+        Network: ["auditor", "developer"],
+        Fairness: ["endUser", "developer"],
+    };
 
-  const currentRoles = rolesForServices[currentService] || [];
+    const currentRoles = rolesForServices[currentService] || [];
 
-  return (
-    <RoleContext.Provider
-      value={{
-        roles: currentRoles,
-        currentService,
-        setCurrentService,
-        userRole,
-        setUserRole,
-      }}
-    >
-      {children}
-    </RoleContext.Provider>
-  );
+    return (
+        <RoleContext.Provider
+            value={{
+                roles: currentRoles,
+                currentService,
+                setCurrentService,
+                userRole,
+                setUserRole,
+            }}
+        >
+            {children}
+        </RoleContext.Provider>
+    );
 };
 
 export const useRoleContext = (): RoleContextProps => {
-  const context = useContext(RoleContext);
-  if (!context) {
-    throw new Error("useRoleContext must be used within a RoleProvider");
-  }
-  return context;
+    const context = useContext(RoleContext);
+    if (!context) {
+        throw new Error("useRoleContext must be used within a RoleProvider");
+    }
+    return context;
 };
--- src/components/RoleProvider/RoleSelector.tsx
+++ src/components/RoleProvider/RoleSelector.tsx
@@ -1,47 +1,47 @@
-import React, { useEffect } from "react";
-import { useRoleContext, Role } from "./RoleContext"; // Import Role type
+import React, {useEffect} from "react";
+import {useRoleContext, Role} from "./RoleContext"; // Import Role type
 
 const roleNames: { [key in Role]: string } = {
-  // user: "User",
-  developer: "Developer",
-  auditor: "Auditor",
-  medicalExpert: "Medical Expert",
-  business: "Business",
-  endUser: "End User",
+    // user: "User",
+    developer: "Developer",
+    auditor: "Auditor",
+    medicalExpert: "Medical Expert",
+    business: "Business",
+    endUser: "End User",
 };
 
 const RoleSelector: React.FC = () => {
-  const { roles, userRole, setUserRole } = useRoleContext();
+    const {roles, userRole, setUserRole} = useRoleContext();
 
-  useEffect(() => {
-    if (roles.length > 0 && !roles.includes(userRole)) {
-      setUserRole(roles[0]); // Set default role if current role is not in the new roles list
-    }
-  }, [roles, userRole, setUserRole]);
+    useEffect(() => {
+        if (roles.length > 0 && !roles.includes(userRole)) {
+            setUserRole(roles[0]); // Set default role if current role is not in the new roles list
+        }
+    }, [roles, userRole, setUserRole]);
 
-  const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setUserRole(event.target.value as Role);
-  };
+    const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setUserRole(event.target.value as Role);
+    };
 
-  return (
-    <div className="mb-3">
-      <label htmlFor="userRoleSelector" className="form-label">
-        <h5>User Role:</h5>
-      </label>
-      <select
-        id="userRoleSelector"
-        className="form-select"
-        value={userRole}
-        onChange={handleRoleChange}
-      >
-        {roles.map((role) => (
-          <option key={role} value={role}>
-            {roleNames[role]}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+    return (
+        <div className="mb-3">
+            <label htmlFor="userRoleSelector" className="form-label">
+                <h5>User Role:</h5>
+            </label>
+            <select
+                id="userRoleSelector"
+                className="form-select"
+                value={userRole}
+                onChange={handleRoleChange}
+            >
+                {roles.map((role) => (
+                    <option key={role} value={role}>
+                        {roleNames[role]}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 };
 
 export default RoleSelector;
