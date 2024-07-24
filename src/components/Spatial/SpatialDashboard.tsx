c1f26ca14dc0569137fd908312896cb24616ee45
elemson <toluwanielemosho@gmail.com>
2024-07-24 12:02:41 +0100

    Refactor: Handle undefined params data

--- src/components/Spatial/SpatialDashboard.tsx
+++ src/components/Spatial/SpatialDashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router";
 import { Container } from "react-bootstrap";
 import MedicalDashboard from "./Dashboard/MedicalDashboard";
@@ -7,8 +7,8 @@ import NetworkDashboard from "./Dashboard/NetworkDashboard";
 const SpatialDashboard: React.FC = () => {
   const { modelId: routeModelId } = useParams();
 
-  const getFirstTwoCharsOfModelId = (params: string): string =>
-      params.length >= 2 ? params.substring(0, 2) : "";
+  const getFirstTwoCharsOfModelId = (params: string | undefined): string =>
+      params && params.length >= 2 ? params.substring(0, 2) : params ? params : "";
 
   const serviceMapping: { [key: string]: string } = {
     ac: "Network",
@@ -20,8 +20,8 @@ const SpatialDashboard: React.FC = () => {
   return (
       <Container className="mt-5">
         <h2>Spatial Dashboard</h2>
-        {serviceType === "Medical" && <MedicalDashboard />}
-        {serviceType === "Network" && <NetworkDashboard />}
+        {serviceType === "Medical" && <MedicalDashboard/>}
+        {serviceType === "Network" && <NetworkDashboard/>}
       </Container>
   );
 };
